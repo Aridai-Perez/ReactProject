fc6a2ce4fd6667fd0629092e0d9aefc991e55312
Yithzabi Aridai F. Perez <141900042+Aridai-Perez@users.noreply.github.com>
2024-01-24 11:32:46 -0800

    about me agregado

---
+++ src/Components/About.jsx
@@ -0,0 +1,16 @@
+import './About.css'
+
+function About(){
+    return ( 
+    <div className='about-container'>
+        <h2>About me</h2>
+        <img src="../images/fotito-mia.jpg" alt="" width={200} />
+        <h3>Aridai F. Perez</h3>
+        <p>Amante de los perritos y una programadora en proceso.</p>
+        <p>Me encanta programar con musica a todo volumen y seguir aprendiendo en el proceso.</p>
+        <h4>Contacto: aridaiperez03@gmail.com</h4>
+    </div>
+    )
+}
+
+export default About
